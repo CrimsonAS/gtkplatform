8ef5b11cd440126b40e2d45f1d1f7a19211cd0be
John Brooks <john@jbrooks.io>
2017-09-27 12:30:53 -0600

    QGtkOpenGLContext: Properly request and query wayland GL version
    Fixes #24

--- src/qgtkopenglcontext_wayland.cpp
+++ src/qgtkopenglcontext_wayland.cpp
@@ -46,6 +46,8 @@
 #include <QLoggingCategory>
 #include "CSystrace.h"
 
+static QSurfaceFormat qgtk_wayland_update_format(const QSurfaceFormat &refFormat, EGLDisplay display, EGLContext context);
+
 void QGtkWaylandContext::initialize()
 {
     QGtkIntegration *integration = QGtkIntegration::instance();
@@ -58,10 +60,100 @@ void QGtkWaylandContext::initialize()
         shareEgl = static_cast<QGtkWaylandContext*>(m_shareContext)->eglContext();
 
     m_eglConfig = q_configFromGLFormat(m_eglDisplay, m_format);
+
+    QVector<EGLint> eglContextAttrs;
+    eglContextAttrs.append(EGL_CONTEXT_CLIENT_VERSION);
+    eglContextAttrs.append(m_format.majorVersion());
+    const bool hasKHRCreateContext = q_hasEglExtension(m_eglDisplay, "EGL_KHR_create_context");
+    if (hasKHRCreateContext) {
+        eglContextAttrs.append(EGL_CONTEXT_MINOR_VERSION_KHR);
+        eglContextAttrs.append(m_format.minorVersion());
+        int flags = 0;
+        // The debug bit is supported both for OpenGL and OpenGL ES.
+        if (m_format.testOption(QSurfaceFormat::DebugContext))
+            flags |= EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR;
+        // The fwdcompat bit is only for OpenGL 3.0+.
+        if (m_format.renderableType() == QSurfaceFormat::OpenGL
+            && m_format.majorVersion() >= 3
+            && !m_format.testOption(QSurfaceFormat::DeprecatedFunctions))
+            flags |= EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR;
+        if (flags) {
+            eglContextAttrs.append(EGL_CONTEXT_FLAGS_KHR);
+            eglContextAttrs.append(flags);
+        }
+        // Profiles are OpenGL only and mandatory in 3.2+. The value is silently ignored for < 3.2.
+        if (m_format.renderableType() == QSurfaceFormat::OpenGL) {
+            eglContextAttrs.append(EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR);
+            eglContextAttrs.append(m_format.profile() == QSurfaceFormat::CoreProfile
+                                ? EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT_KHR
+                                : EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR);
+        }
+    }
+    eglContextAttrs.append(EGL_NONE);
+
     m_format = q_glFormatFromConfig(m_eglDisplay, m_eglConfig, m_format);
     m_eglContext = eglCreateContext(m_eglDisplay, m_eglConfig,
-                                    shareEgl, NULL);
-    Q_ASSERT(m_eglContext);
+                                    shareEgl, eglContextAttrs.data());
+
+	if (m_eglContext)
+        m_format = qgtk_wayland_update_format(m_format, m_eglDisplay, m_eglContext);
+}
+
+// Gently borrowed from the wayland QPA plugin; this code appears in several places,
+// it should really be put into eglconvenience instead.
+static QSurfaceFormat qgtk_wayland_update_format(const QSurfaceFormat &refFormat, EGLDisplay display, EGLContext context)
+{
+	QSurfaceFormat format = refFormat;
+
+    // Have to save & restore to prevent QOpenGLContext::currentContext() from becoming
+    // inconsistent after QOpenGLContext::create().
+    EGLDisplay prevDisplay = eglGetCurrentDisplay();
+    if (prevDisplay == EGL_NO_DISPLAY) // when no context is current
+        prevDisplay = display;
+    EGLContext prevContext = eglGetCurrentContext();
+    EGLSurface prevSurfaceDraw = eglGetCurrentSurface(EGL_DRAW);
+    EGLSurface prevSurfaceRead = eglGetCurrentSurface(EGL_READ);
+
+    if (eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, context)) {
+        if (format.renderableType() == QSurfaceFormat::OpenGL
+            || format.renderableType() == QSurfaceFormat::OpenGLES) {
+            const GLubyte *s = glGetString(GL_VERSION);
+            if (s) {
+                QByteArray version = QByteArray(reinterpret_cast<const char *>(s));
+                int major, minor;
+                if (QPlatformOpenGLContext::parseOpenGLVersion(version, major, minor)) {
+                    format.setMajorVersion(major);
+                    format.setMinorVersion(minor);
+                }
+            }
+            format.setProfile(QSurfaceFormat::NoProfile);
+            format.setOptions(QSurfaceFormat::FormatOptions());
+            if (format.renderableType() == QSurfaceFormat::OpenGL) {
+                // Check profile and options.
+                if (format.majorVersion() < 3) {
+                    format.setOption(QSurfaceFormat::DeprecatedFunctions);
+                } else {
+                    GLint value = 0;
+                    glGetIntegerv(GL_CONTEXT_FLAGS, &value);
+                    if (!(value & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT))
+                        format.setOption(QSurfaceFormat::DeprecatedFunctions);
+                    if (value & GL_CONTEXT_FLAG_DEBUG_BIT)
+                        format.setOption(QSurfaceFormat::DebugContext);
+                    if (format.version() >= qMakePair(3, 2)) {
+                        value = 0;
+                        glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &value);
+                        if (value & GL_CONTEXT_CORE_PROFILE_BIT)
+                            format.setProfile(QSurfaceFormat::CoreProfile);
+                        else if (value & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT)
+                            format.setProfile(QSurfaceFormat::CompatibilityProfile);
+                    }
+                }
+            }
+        }
+        eglMakeCurrent(prevDisplay, prevSurfaceDraw, prevSurfaceRead, prevContext);
+    }
+
+    return format;
 }
 
 QGtkWaylandContext::~QGtkWaylandContext()
